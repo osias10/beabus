eba84cdcc4211b9d80f0743d2744a8c441420507
osias10 <wnsguddk1@naver.com>
2022-10-09 13:01:34 +0900

    initialize beabus

---
+++ src/main/java/org/duckdns/osias/beabus/BeabusApplication.java
@@ -0,0 +1,13 @@
+package org.duckdns.osias.beabus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeabusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeabusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/duckdns/osias/beabus/BeabusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.duckdns.osias.beabus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeabusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
